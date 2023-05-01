ef9918f76d00b874c1b0d720402698cf637b35a6
Dott <chenyongarch@gmail.com>
2023-05-01 18:58:28 +0800

    fix: text color

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,17 @@ export default function Home() {
     <>
       <Head>
         <title>Lovin</title>
-        <meta name="description" content="A small software studio making delightful apps to make your life better." />
+        <meta
+          name="description"
+          content="A small software studio making delightful apps to make your life better."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/lovin-logo.png" />
       </Head>
       <main className="flex h-screen w-full items-center justify-center gap-4">
-        <p className="text-base font-normal">We make wonderful things.</p>
+        <p className="text-base font-normal text-[var(--bg-faint)]">
+          We make wonderful things.
+        </p>
       </main>
     </>
   );
