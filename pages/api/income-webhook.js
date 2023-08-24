d8ae645bb31273cc30d6dc34eb0239a2d91e1a9c
Dott <chenyongarch@gmail.com>
2023-08-25 00:26:17 +0800

    feat: add discord stripe webhook

---
+++ pages/api/income-webhook.js
@@ -0,0 +1,53 @@
+import { Client } from 'discord.js';
+
+const discordClient = new Client({ intents: ["Guilds"] });
+// MARK: Confidential
+const discordToken = 'MTE0NDI1NDIyNTE2MTkyODgxNQ.GBWyWl.qAWLTOYfFFiJ1uSmchFczFipWU6UzxazQGGPrM';
+const discordChannelId = '1143902845112942714';
+
+discordClient.login(discordToken);
+discordClient.on('ready', () => {
+    console.log(`Logged in as ${discordClient.user.tag}!`);
+})
+
+export default async (req, res) => {
+    if (req.method === 'POST') {
+        let event;
+
+        try {
+            event = req.body;
+        } catch (err) {
+            res.status(400).send(`Webhook Error: ${err.message}`);
+            return;
+        }
+
+        // Handle the event
+        console.log(`Received event: ${event.type}`);
+        switch (event.type) {
+            case 'charge.succeeded':
+                const charge = event.data.object;
+                const productName = charge.payment_intent === null ? "OpenAIKey" : "书译翻译服务"
+                const amount = charge.amount / 100;
+                const notification = `恭喜发财！🤑 用户购买了产品“${productName}”，你又赚了${amount}元！💰 真棒啊！👍`;
+                console.log(notification);
+                // Send a message to a Discord channel
+                const channel = discordClient.channels.cache.get(discordChannelId);
+                if (channel) {
+                    channel.send(notification);
+                    console.log("Message sent!")
+                } else {
+                    console.log("Channel not found!")
+                }
+                break;
+            default:
+                console.log(`Unhandled event type ${event.type}`);
+        }
+
+        // Return a response to acknowledge receipt of the event
+        res.json({ received: true });
+    } else {
+        // Handle any other HTTP method
+        res.setHeader('Allow', ['POST'])
+        res.status(405).end(`Method ${req.method} Not Allowed`)
+    }
+};
